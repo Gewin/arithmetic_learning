09c696900cec9e37c490de0c746eaf8c430b04ae
Gewin <ljwlh54@163.com>
2014-03-20 22:40:54 +0800

    Add selection sort function

--- Arithmetic/arithmetic_learning.cpp
+++ Arithmetic/arithmetic_learning.cpp
@@ -1,44 +1,88 @@
 #include "stdio.h"
+#include "string.h"
 
-void BubbleSort(void)
+#define MAX_NUM       10
+
+void BubbleSort(int *data)
 {
-	int a[10] = {54, 3, 7, 5, 12, 13, 1232, 8657, 96865, 434343};
-
 	int i, j;
+	int tempData[MAX_NUM];
 	int temp = 0;
 
-	printf("Before sort : \n");
-	for(i = 0; i < 10; i++)
-	{
-		printf("%d ", a[i]);
-	}
-	printf("\n");
+	printf("Begin bubble sort\n");
+
+
+	memcpy(tempData, data, MAX_NUM * sizeof(int));
 
 	for(j = 0; j < 10; j++)
 	{
 		for(i = 0; i < 9 - j; i++)
 		{
-			if(a[i] < a[i+1])
+			if(tempData[i] < tempData[i+1])
 			{
-				temp = a[i];
-				a[i] = a[i+1];
-				a[i+1] = temp;
+				temp = tempData[i];
+				tempData[i] = tempData[i+1];
+				tempData[i+1] = temp;
 			}
 		}
 	}
-	printf("After sort: \n");
+	printf("After bubble sort: \n");
 	for(i = 0; i < 10; i++)
 	{
-		printf("%d ", a[i]);
+		printf("%d ", tempData[i]);
 	}
-	printf("\n");
+	printf("\n\n");
+}
 
+void SelectionSort(int *data, int length)
+{
+	int temp;
+	int i, j, index;
+	int tempData[MAX_NUM];
+
+	printf("Begin selection sort\n");
+
+	memcpy(tempData, data, MAX_NUM * sizeof(int));
+
+	for(i = 0; i < length; i++)
+	{
+		int index = i;
+		for(j = i + 1; j < length; j++)
+		{
+			if(tempData[j] < tempData[index])
+				index = j;
+		}
+		if(index != i)
+		{
+			temp = tempData[i]; tempData[i] = tempData[index]; tempData[index] = temp;
+		}
+	}
+	printf("After selection sort: \n");
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d ", tempData[i]);
+	}
+	printf("\n\n");
 }
 
 int main(void)
 {
+	int i;
 	printf("Begin my road!\n");
 
+	int data[MAX_NUM] = {54, 3, 7, 5, 12, 13, 1232, 8657, 96865, 434343};
+
+	printf("Before sort : \n");
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+
+
 	/* 2014.3.18 : Bubble sort*/
-	BubbleSort();
+	BubbleSort(data);
+
+	/* 2014.3.20 : Selection sort */
+	SelectionSort(data, MAX_NUM);
 }
